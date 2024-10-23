cd9f8021eb60d614f7cd2fb76d6677bf96274f61
stighellemans <stighellemans@gmail.com>
2024-10-23 14:02:18 +0200

    d norm fix

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from typing import Callable, Dict, List, Sequence, TypeVar
+from math import log10, sqrt
+from typing import Callable, Dict, List, TypeVar
 
 from .helpers import tokens_to_term_freqs
 
@@ -14,6 +15,13 @@ class Posting:
 
 
 @dataclass
+class PositionalPosting:
+    # later term_freq_td -> positional indices
+    doc_id: DocID_T
+    term_freq_td: int
+
+
+@dataclass
 class TermInfo:
     doc_freq_t: int
     posting_list: List["Posting"]
@@ -22,7 +30,6 @@ class TermInfo:
 class Database:
     inverted_index: Dict[str, TermInfo]
     doc_normalizations: Dict[DocID_T, int]
-    doc_ids: List[DocID_T]
 
     def __init__(
         self, tokenize_fn: Callable[[str], List[str]], docs: Dict[DocID_T, str]
@@ -33,15 +40,17 @@ class Database:
         """
         self.tokenize = tokenize_fn
         self.inverted_index = {}
-        self.doc_ids = []
+        self.doc_normalizations = {}
 
         for doc_id, doc in docs.items():
             self.update(doc_id, doc)
 
+        self.precompute_doc_norms()
+
     def update(self, doc_id: DocID_T, doc: str):
         """updates the database"""
-        if doc_id not in self.doc_ids:
-            self.doc_ids.append(doc_id)
+        if doc_id not in self.doc_normalizations.keys():
+            self.doc_normalizations[doc_id] = None
         else:
             # skip duplicates
             return
@@ -59,6 +68,31 @@ class Database:
                 # include new doc
                 self.inverted_index[term].posting_list.append(Posting(doc_id, freq_td))
 
+        # doc norms already computed -> update norms
+        if all(d_norm is not None for d_norm in self.doc_normalizations.values()):
+            self.precompute_doc_norms()
+
+    def precompute_doc_norms(self):
+        """precomputes doc norms"""
+        # reset doc norms
+        self.doc_normalizations = {
+            doc_id: 0 for doc_id in self.doc_normalizations.keys()
+        }
+
+        # accumulate weights + root of total
+        for term_info in self.inverted_index.values():
+            df_t = term_info.doc_freq_t
+            for post in term_info.posting_list:
+                doc_id = post.doc_id
+                tf_td = post.term_freq_td
+
+                w_td = (1 + log10(tf_td)) * log10(self.db_size() / df_t)
+                self.doc_normalizations[doc_id] += w_td**2
+
+        self.doc_normalizations = {
+            doc_id: sqrt(w) for doc_id, w in self.doc_normalizations.items()
+        }
+
     def remove(self, id: DocID_T):
         """removes a doc from the database"""
         pass
@@ -69,4 +103,83 @@ class Database:
 
     def db_size(self):
         """returns database size"""
-        return len(self.doc_ids)
+        return len(list(self.doc_normalizations))
+
+
+class PositionalDatabase:
+    inverted_index: Dict[str, TermInfo]
+    doc_normalizations: Dict[DocID_T, PositionalPosting]
+
+    def __init__(
+        self, tokenize_fn: Callable[[str], List[str]], docs: Dict[DocID_T, str]
+    ):
+        """Input:
+        - tokenize_fn for this database
+        - dictionary: docID -> text
+        """
+        self.tokenize = tokenize_fn
+        self.inverted_index = {}
+        self.doc_normalizations = {}
+
+        for doc_id, doc in docs.items():
+            self.update(doc_id, doc)
+
+        self.precompute_doc_norms()
+
+    def update(self, doc_id: DocID_T, doc: str):
+        """updates the database"""
+        if doc_id not in self.doc_normalizations.keys():
+            self.doc_normalizations[doc_id] = None
+        else:
+            # skip duplicates
+            return
+
+        tokens = self.tokenize(doc)
+        term_freqs = tokens_to_term_freqs(tokens)
+
+        for term, freq_td in term_freqs.items():
+            if term not in self.inverted_index:
+                # start with: one doc has this term
+                self.inverted_index[term] = TermInfo(1, [Posting(doc_id, freq_td)])
+            else:
+                # term is in one doc more
+                self.inverted_index[term].doc_freq_t += 1
+                # include new doc
+                self.inverted_index[term].posting_list.append(Posting(doc_id, freq_td))
+
+        # doc norms already computed -> update norms
+        if all(d_norm is not None for d_norm in self.doc_normalizations.values()):
+            self.precompute_doc_norms()
+
+    def precompute_doc_norms(self):
+        """precomputes doc norms"""
+        # reset doc norms
+        self.doc_normalizations = {
+            doc_id: 0 for doc_id in self.doc_normalizations.keys()
+        }
+
+        # accumulate weights + root of total
+        for term_info in self.inverted_index.values():
+            df_t = term_info.doc_freq_t
+            for post in term_info.posting_list:
+                doc_id = post.doc_id
+                tf_td = post.term_freq_td
+
+                w_td = (1 + log10(tf_td)) * log10(self.db_size() / df_t)
+                self.doc_normalizations[doc_id] += w_td**2
+
+        self.doc_normalizations = {
+            doc_id: sqrt(w) for doc_id, w in self.doc_normalizations.items()
+        }
+
+    def remove(self, id: DocID_T):
+        """removes a doc from the database"""
+        pass
+
+    def vocab_size(self):
+        """returns the vocabulary size"""
+        return len(list(self.inverted_index))
+
+    def db_size(self):
+        """returns database size"""
+        return len(list(self.doc_normalizations))
--- src/query.py
+++ src/query.py
@@ -1,6 +1,8 @@
-from .helpers import tokens_to_term_freqs
-from .database import Database
 from math import log10, sqrt
+
+from .database import Database
+from .helpers import tokens_to_term_freqs
+
 
 def query_database(database: Database, query: str) -> dict:
     q_tokens = database.tokenize(query)
@@ -10,32 +12,29 @@ def query_database(database: Database, query: str) -> dict:
     q_term_freqs = tokens_to_term_freqs(q_tokens)
 
     q_norm_accum = 0
-    doc_accum = {}
+    doc_term_sim = {}
     for q_term, tf_tq in q_term_freqs.items():
         term_info = database.inverted_index[q_term]
         df_t = term_info.doc_freq_t
 
         w_tq = (1 + log10(tf_tq)) * log10(database.db_size() / df_t)
-        q_norm_accum += w_tq ** 2
+        q_norm_accum += w_tq**2
 
         for post in term_info.posting_list:
             doc_id = post.doc_id
             tf_td = post.term_freq_td
-            if doc_id not in doc_accum:
-                doc_accum[doc_id] = {"similarity": 0, "d_norm": 0}
+            if doc_id not in doc_term_sim:
+                doc_term_sim[doc_id] = 0
 
             w_td = (1 + log10(tf_td)) * log10(database.db_size() / df_t)
-            doc_accum[doc_id]["similarity"] += w_td * w_tq
-
-            # d normalization
-            doc_accum[doc_id]["d_norm"] += w_td ** 2
+            doc_term_sim[doc_id] += w_td * w_tq
 
     q_norm = sqrt(q_norm_accum)
 
     similarities = []
-    for doc_id, accum in doc_accum.items():
-        d_norm = sqrt(accum["d_norm"])
-        similarity = accum["similarity"] / (d_norm * q_norm)
+    for doc_id, sim_score in doc_term_sim.items():
+        d_norm = database.doc_normalizations[doc_id]
+        similarity = sim_score / (d_norm * q_norm)
         similarities.append((doc_id, similarity))
 
     return sorted(similarities, key=lambda x: x[1], reverse=True)
