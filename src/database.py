718e0069fad47ade99777c4da41af83780430e78
stighellemans <stighellemans@gmail.com>
2024-10-23 14:27:32 +0200

    small changes

--- src/database.py
+++ src/database.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
 from math import log10, sqrt
-from typing import Callable, Dict, List, TypeVar
+from typing import Callable, Dict, List, TypeVar, Union
 
-from .helpers import tokens_to_term_freqs
+from .helpers import tokens_to_pos_idxs, tokens_to_term_freqs
 
 DocID_T = TypeVar("DocID_T", str, int)
 
@@ -18,13 +18,13 @@ class Posting:
 class PositionalPosting:
     # later term_freq_td -> positional indices
     doc_id: DocID_T
-    term_freq_td: List[int]
+    pos_idxs: List[int]
 
 
 @dataclass
 class TermInfo:
     doc_freq_t: int
-    posting_list: List["Posting"]
+    posting_list: List[Union["Posting", "PositionalPosting"]]
 
 
 class Database:
@@ -56,17 +56,17 @@ class Database:
             return
 
         tokens = self.tokenize(doc)
-        term_freqs = tokens_to_term_freqs(tokens)
+        term_pos_idxs = tokens_to_pos_idxs(tokens)
 
-        for term, freq_td in term_freqs.items():
+        for term, pos_idxs in term_pos_idxs.items():
             if term not in self.inverted_index:
                 # start with: one doc has this term
-                self.inverted_index[term] = TermInfo(1, [Posting(doc_id, freq_td)])
+                self.inverted_index[term] = TermInfo(1, [Posting(doc_id, pos_idxs)])
             else:
                 # term is in one doc more
                 self.inverted_index[term].doc_freq_t += 1
                 # include new doc
-                self.inverted_index[term].posting_list.append(Posting(doc_id, freq_td))
+                self.inverted_index[term].posting_list.append(Posting(doc_id, pos_idxs))
 
         # doc norms already computed -> update norms
         if all(d_norm is not None for d_norm in self.doc_normalizations.values()):
@@ -140,12 +140,16 @@ class PositionalDatabase:
         for term, freq_td in term_freqs.items():
             if term not in self.inverted_index:
                 # start with: one doc has this term
-                self.inverted_index[term] = TermInfo(1, [Posting(doc_id, freq_td)])
+                self.inverted_index[term] = TermInfo(
+                    1, [PositionalPosting(doc_id, freq_td)]
+                )
             else:
                 # term is in one doc more
                 self.inverted_index[term].doc_freq_t += 1
                 # include new doc
-                self.inverted_index[term].posting_list.append(Posting(doc_id, freq_td))
+                self.inverted_index[term].posting_list.append(
+                    PositionalPosting(doc_id, freq_td)
+                )
 
         # doc norms already computed -> update norms
         if all(d_norm is not None for d_norm in self.doc_normalizations.values()):
@@ -163,9 +167,9 @@ class PositionalDatabase:
             df_t = term_info.doc_freq_t
             for post in term_info.posting_list:
                 doc_id = post.doc_id
-                tf_td = post.term_freq_td
+                pos_idxs = post.pos_idxs
 
-                w_td = (1 + log10(tf_td)) * log10(self.db_size() / df_t)
+                w_td = (1 + log10(len(pos_idxs))) * log10(self.db_size() / df_t)
                 self.doc_normalizations[doc_id] += w_td**2
 
         self.doc_normalizations = {
--- src/query.py
+++ src/query.py
@@ -32,25 +32,27 @@ def query_database(database: Database, query: str) -> dict:
     q_norm = sqrt(q_norm_accum)
 
     similarities = []
-    for doc_id, sim_score in doc_term_sim.items():
+    for doc_id, raw_sim in doc_term_sim.items():
         d_norm = database.doc_normalizations[doc_id]
-        similarity = sim_score / (d_norm * q_norm)
+        similarity = raw_sim / (d_norm * q_norm)
         similarities.append((doc_id, similarity))
 
     return sorted(similarities, key=lambda x: x[1], reverse=True)
 
 
-def pos_query_database(database: Database, query: str, alpha: float) -> dict:
+def pos_query_database(
+    database: Database, query: str, boost: float, width: int, term_fraction
+) -> dict:
     q_tokens = database.tokenize(query)
     # remove tokens not in vocab
     q_tokens = [term for term in q_tokens if term in database.inverted_index]
 
     q_term_freqs = tokens_to_term_freqs(q_tokens)
 
-    min_dists = calc_min_distances(q_tokens, database)
+    # min_dists = calc_min_distances(q_tokens, database)
 
     q_norm_accum = 0
-    doc_accum = {}
+    doc_term_sim = {}
     for q_term, tf_tq in q_term_freqs.items():
         term_info = database.inverted_index[q_term]
         df_t = term_info.doc_freq_t
@@ -61,23 +63,20 @@ def pos_query_database(database: Database, query: str, alpha: float) -> dict:
         for post in term_info.posting_list:
             doc_id = post.doc_id
             tf_td = len(post.pos_idxs)
-            if doc_id not in doc_accum:
-                doc_accum[doc_id] = {"similarity": 0, "d_norm": 0}
+            if doc_id not in doc_term_sim:
+                doc_term_sim[doc_id] = 0
 
-            pos_w = log10(alpha + exp(-min_dists[doc_id]))
+            # pos_w = log10(alpha + exp(-min_dists[doc_id]))
 
             w_td = (1 + log10(tf_td)) * log10(database.db_size() / df_t)
-            doc_accum[doc_id]["similarity"] += w_td * w_tq + pos_w
-
-            # d normalization
-            doc_accum[doc_id]["d_norm"] += w_td**2
+            doc_term_sim[doc_id] += w_td * w_tq
 
     q_norm = sqrt(q_norm_accum)
 
     similarities = []
-    for doc_id, accum in doc_accum.items():
-        d_norm = sqrt(accum["d_norm"])
-        similarity = accum["similarity"] / (d_norm * q_norm)
+    for doc_id, raw_sim in doc_term_sim.items():
+        d_norm = database.doc_normalizations[doc_id]
+        similarity = raw_sim / (d_norm * q_norm)
         similarities.append((doc_id, similarity))
 
     return sorted(similarities, key=lambda x: x[1], reverse=True)
